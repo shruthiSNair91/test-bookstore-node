f7b10b87498e99526b3a98038ab6cdba43599298
shruthiSNair91 <shruthi.snair91@gmail.com>
2025-01-23 09:33:31 +0530

    DB connection string updated

--- database/db.js
+++ database/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
         try{
-                await mongoose.connect("mongodb://127.0.0.1:27017/bookstore");
+                await mongoose.connect(" mongodb://localhost:27017/bookstore");
+                // await mongoose.connect("mongodb://127.0.0.1:27017/bookstore");
                 console.log('DB connected succesfully..');                
         }catch(err){
                 console.error('Mongo DB connection Failed..',err);
